2d4b4fcb853651a5a24e1e24d087f059c996788a
cirusuofr <ccw292@uregina.ca>
2025-06-27 16:22:10 -0600

    round 1

--- test/DateValidatorTest.java
+++ test/DateValidatorTest.java
@@ -1,3 +1,13 @@
+import org.junit.jupiter.api.Test; //JUnit5
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 public class DateValidatorTest {
     
+    @Test
+    public void Validate_Today_True(){
+        DateValidator dateValidator = new DateValidator();
+        boolean legalDate = dateValidator.validate("06202025");
+        assertTrue(legalDate);
+
+    }
 }
