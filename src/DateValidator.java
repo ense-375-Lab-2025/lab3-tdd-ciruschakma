c2ab47c1e9a766a8419248803e5159a4d73a41b9
cirusuofr <ccw292@uregina.ca>
2025-06-27 17:02:03 -0600

    Round 1

--- src/DateValidator.java
+++ src/DateValidator.java
@@ -1,3 +1,9 @@
 public class DateValidator {
-    
+    public boolean validate(String date){
+        if (date == "06202025"){
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
