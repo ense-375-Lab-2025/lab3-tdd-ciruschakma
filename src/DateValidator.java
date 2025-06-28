47230c00881696f6a61ea7722c170a1dbf30a4f7
cirusuofr <ccw292@uregina.ca>
2025-06-27 20:34:00 -0600

    Round 4 Green

--- src/DateValidator.java
+++ src/DateValidator.java
@@ -9,10 +9,16 @@ public class DateValidator {
             int day = Integer.parseInt(dd);
             int year = Integer.parseInt(yyyy);
 
-            if (day >= 32 || month >= 13) {
-                return false;
-            }
-
+            if ( day >= 32 || month >= 13 ||
+                (month == 2  && day == 31) ||
+                (month == 4  && day == 31) ||
+                (month == 6  && day == 31) ||
+                (month == 9  && day == 31) ||
+                (month == 11 && day == 31) ||
+                (month == 2  && day == 30) ||
+                (month == 2  && day == 29) ) {
+                    return false;
+                }
         } else {
             return false;
         }
