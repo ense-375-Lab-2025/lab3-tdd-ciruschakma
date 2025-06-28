e3386b5f000cf970f14b880df4db5a619adac4a5
cirusuofr <ccw292@uregina.ca>
2025-06-27 20:32:41 -0600

    Round 3 Green

--- src/DateValidator.java
+++ src/DateValidator.java
@@ -1,9 +1,21 @@
 public class DateValidator {
     public boolean validate(String date){
         if (date.length() == 8){
-            return true;
+            String mm = date.substring(0,2);
+            String dd = date.substring(2,4);
+            String yyyy = date.substring(4,8);
+
+            int month = Integer.parseInt(mm);
+            int day = Integer.parseInt(dd);
+            int year = Integer.parseInt(yyyy);
+
+            if (day >= 32 || month >= 13) {
+                return false;
+            }
+
         } else {
             return false;
         }
+        return true;
     }
 }
